6382b51b768da992a751bfcb28ab7294756961a1
José Vicente Sevillano <sevi.82@gmail.com>
2015-07-26 13:11:10 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,13 +19,13 @@ exports.index = function(req, res) {
     var cadena = '%' + req.query.search.replace(/ /g, "%") + '%';
     models.Quiz.findAll({where:["pregunta like ?", cadena]}).then(
       function(quizes){
-        res.render('quizes/index', {quizes: quizes});
+        res.render('quizes/index', {quizes: quizes, errors: []});
       }
     ).catch(function(error) { next(error);})
   } else {
     models.Quiz.findAll().then(
       function(quizes) {
-        res.render('quizes/index.ejs', { quizes: quizes});
+        res.render('quizes/index.ejs', { quizes: quizes, errors: []});
       }
     ).catch(function(error) { next(error);})
   }
@@ -33,7 +33,7 @@ exports.index = function(req, res) {
 
 // GET  /quizes?search=texto_a_buscar
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -42,7 +42,11 @@ exports.answer = function(req, res) {
   if(req.query.respuesta === req.quiz.respuesta){
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render(
+    'quizes/answer',
+    { quiz: req.quiz,
+      respuesta: resultado,
+      errors: []});
 };
 
 // GET /quizes/new
@@ -51,15 +55,22 @@ exports.new = function(req, res) {
     {pregunta: "Pregtunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })	//Redirección HTTP (URL relativo) lista de preguntas
+  var errors = quiz.validate();
+  if(errors){
+    var i=0;
+    var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+    for (var prop in errors) errores[i++]={message: errors[prop]};	
+    res.render('quizes/new', {quiz: quiz, errors: errores});
+  } else {
+    quiz // save: guarda en DB campos pregunta y respuesta de quiz
+    .save({fields: ["pregunta", "respuesta"]})
+    .then( function(){res.redirect('/quizes')})
+  }         // res.redirect: Redirección HTTP a lista de preguntas
 };
