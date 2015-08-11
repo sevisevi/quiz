d717ee3b58280922809eecddc42ab50d514982b1
José Vicente Sevillano <sevi.82@gmail.com>
2015-08-11 18:12:04 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);	// autoload :quizId
+router.param('commentId', commentController.load);	// autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login',	sessionController.new);		// formulario login
@@ -32,7 +33,9 @@ router.get('/quizes/:quizId(\\d+)/edit',	sessionController.loginRequired, quizCo
 router.put('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',								sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
